b72318cf031d42725c331802adb8986c72c21b28
renan <renanalves23@outlook.com>
2020-02-23 16:55:46 -0300

    Commit Inicial

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.display}>Display</Text>
+      <Text style={styles.result}>Result</Text>
+      <View style={styles.button}></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttons:{
+
+  },
+});
