0e507ec883c677fdda468f08453ff928673af839
renan <renanalves23@outlook.com>
2020-02-23 17:54:40 -0300

    Layout Calculadora antes do colocar os numeros

--- App.js
+++ App.js
@@ -6,7 +6,14 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.display}>Display</Text>
       <Text style={styles.result}>Result</Text>
-      <View style={styles.button}></View>
+      <View style={styles.buttons}>
+          <View style={styles.col1}>
+            <Text>Col1</Text>
+          </View>
+        <View style={styles.col2}>
+          <Text>Col2</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,10 +22,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
-  buttons:{
-
+  display:{
+    flex: 1,
+    backgroundColor: '#EFEFEF',
+    fontSize: 80,
+    textAlign: 'right',
+    paddingTop: 30,
+    paddingRight: 10,
   },
-});
+  result: {
+    flex: 0.4,
+    backgroundColor: '#EFEFEF',
+    fontSize: 40,
+    textAlign: 'right',
+    paddingRight: 10,
+    paddingBottom: 10,
+  },
+  buttons:{
+    flex: 5,
+    flexDirection: 'row',
+  },
+  col1:{
+    flex: 3,
+    backgroundColor: 'grey',
+  },
+  col2:{
+    flex: 1,
+    backgroundColor: 'red',
+  },
+
+}); 
